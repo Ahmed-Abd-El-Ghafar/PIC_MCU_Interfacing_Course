0704ef26d4e404ade50dc41c6a40f7749e51d22c
Ahmed Elghafar <Ahmed.Elghafar@AVELABS.CAI>
2020-09-19 14:56:03 +0200

    Adding First try

---
+++ 04 MPLAB X IDE LED and INT0 INT1 External Interrupt Source Code/build/default/production/main.p1.d
@@ -0,0 +1,7 @@
+build/default/production/main.p1:  \
+main.c  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/device_config.h  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/interrupt_manager.h  \
+mcc_generated_files/ext_int.h 
---
+++ 04 MPLAB X IDE LED and INT0 INT1 External Interrupt Source Code/mcc_generated_files/ext_int.c
@@ -0,0 +1,109 @@
+/**
+   EXT_INT Generated Driver File
+ 
+   @Company
+     Microchip Technology Inc.
+ 
+   @File Name
+     ext_int.c
+ 
+   @Summary
+     This is the generated driver implementation file for the EXT_INT driver using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+ 
+   @Description
+     This source file provides implementations for driver APIs for EXT_INT.
+     Generation Information :
+         Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.5
+         Device            :  PIC18F46K20
+         Driver Version    :  1.11
+     The generated drivers are tested against the following:
+         Compiler          :  XC8 2.20 and above
+         MPLAB             :  MPLAB X 5.40
+ */ 
+
+ /**
+   Section: Includes
+ */
+#include <xc.h>
+#include "ext_int.h"
+
+void (*INT0_InterruptHandler)(void);
+void (*INT1_InterruptHandler)(void);
+
+void INT0_ISR(void)
+{
+    EXT_INT0_InterruptFlagClear();
+
+    // Callback function gets called everytime this ISR executes
+    INT0_CallBack();    
+}
+
+
+void INT0_CallBack(void)
+{
+    // Add your custom callback code here
+    if(INT0_InterruptHandler)
+    {
+        INT0_InterruptHandler();
+    }
+}
+
+void INT0_SetInterruptHandler(void (* InterruptHandler)(void)){
+    INT0_InterruptHandler = InterruptHandler;
+}
+
+void INT0_DefaultInterruptHandler(void){
+    // add your INT0 interrupt custom code
+    PORTCbits.RC1 =~ PORTCbits.RC1;
+    // or set custom function using INT0_SetInterruptHandler()
+}
+void INT1_ISR(void)
+{
+    EXT_INT1_InterruptFlagClear();
+
+    // Callback function gets called everytime this ISR executes
+    INT1_CallBack();    
+}
+
+
+void INT1_CallBack(void)
+{
+    // Add your custom callback code here
+    if(INT1_InterruptHandler)
+    {
+        INT1_InterruptHandler();
+    }
+}
+
+void INT1_SetInterruptHandler(void (* InterruptHandler)(void)){
+    INT1_InterruptHandler = InterruptHandler;
+}
+
+void INT1_DefaultInterruptHandler(void){
+    // add your INT1 interrupt custom code
+    PORTCbits.RC2 =~ PORTCbits.RC2;
+    // or set custom function using INT1_SetInterruptHandler()
+}
+
+void EXT_INT_Initialize(void)
+{
+    
+    // Clear the interrupt flag
+    // Set the external interrupt edge detect
+    EXT_INT0_InterruptFlagClear();   
+    EXT_INT0_risingEdgeSet();    
+    // Set Default Interrupt Handler
+    INT0_SetInterruptHandler(INT0_DefaultInterruptHandler);
+    EXT_INT0_InterruptEnable();      
+
+    
+    // Clear the interrupt flag
+    // Set the external interrupt edge detect
+    EXT_INT1_InterruptFlagClear();   
+    EXT_INT1_risingEdgeSet();    
+    // Set Default Interrupt Handler
+    INT1_SetInterruptHandler(INT1_DefaultInterruptHandler);
+    EXT_INT1_InterruptEnable();      
+
+}
+
