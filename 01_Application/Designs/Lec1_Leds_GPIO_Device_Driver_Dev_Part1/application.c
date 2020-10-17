cdb9268b502311c1f68b8ad39cb607286effa1cc
Ahmed Elghafar <Ahmed.Elghafar@AVELABS.CAI>
2020-10-17 14:44:19 +0200

    Adding Changes

---
+++ 01_Application/Designs/Lec1_Leds_GPIO_Device_Driver_Dev_Part1/application.c
@@ -0,0 +1,25 @@
+/* 
+ * File:   application.c
+ * Author: Ahmed.Elghafar
+ * https://www.linkedin.com/in/ahmedabdelghafarmohammed/
+ * Created on October 16, 2020, 7:53 PM
+ */
+
+#include "application.h"
+
+int main() {
+    gpio_pin_direction_intialize(PORTC_INDEX, PIN0, DIRECTION_OUTPUT);
+    gpio_pin_direction_intialize(PORTC_INDEX, PIN1, DIRECTION_OUTPUT);
+    gpio_pin_write_value(PORTC_INDEX, PIN0, PIN_LOW);
+    gpio_pin_write_value(PORTC_INDEX, PIN1, PIN_LOW);
+    while(TRUE){
+        gpio_pin_write_value(PORTC_INDEX, PIN0, PIN_HIGH);
+        gpio_pin_write_value(PORTC_INDEX, PIN1, PIN_LOW);
+        __delay_ms(250);
+        gpio_pin_write_value(PORTC_INDEX, PIN0, PIN_LOW);
+        gpio_pin_write_value(PORTC_INDEX, PIN1, PIN_HIGH);
+        __delay_ms(250);
+    }
+    return (EXIT_SUCCESS);
+}
+
